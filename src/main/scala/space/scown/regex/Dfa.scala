f29c9393e9bba484056718a094c21ef437661ac7
Alex Scown <scowny2000@yahoo.co.uk>
2018-10-13 17:59:54 +0100

    Revert "Rewrite matches to be tail recursive"
    This reverts commit e9d347328dadd359c290a6259aadd2fc10c484e7.

--- src/main/scala/space/scown/regex/Dfa.scala
+++ src/main/scala/space/scown/regex/Dfa.scala
@@ -23,27 +23,24 @@ package space.scown.regex
 
 import space.scown.regex.Dfa.TransitionMap
 
-import scala.annotation.tailrec
-
 case class Dfa(startState: State, finalStates: Set[State], transitions: TransitionMap) {
   def matches(s: String): Stream[String] = {
-    @tailrec
-    def helper(s: Vector[Char], state: State, lastResult: Option[String], acc: Vector[Char], next: Vector[Char], stream: Stream[String]): Stream[String] = s match {
+    def helper(s: Vector[Char], state: State, lastResult: Option[String], acc: Vector[Char], next: Vector[Char]): Stream[String] = s match {
       // If there is no more input, return the last result if we have one
-      case Vector() if lastResult.isDefined => stream #::: Stream(lastResult.get)
-      case Vector() => stream
+      case Vector() if lastResult.isDefined => Stream(lastResult.get)
+      case Vector() => Stream()
       case h +: t => transitions.getOrElse(state, Map()).get(h) match {
         // If the next state is final, record the result
-        case Some(nextState) if finalStates.contains(nextState) => helper(t, nextState, Some((acc :+ h).mkString("")), acc :+ h, t, stream)
+        case Some(nextState) if finalStates.contains(nextState) => helper(t, nextState, Some((acc :+ h).mkString("")), acc :+ h, t)
         // If the next state is not final, build the accumulator
-        case Some(nextState) => helper(t, nextState, lastResult, acc :+ h, next, stream)
+        case Some(nextState) => helper(t, nextState, lastResult, acc :+ h, next)
         // If there is no next state, return a Stream containing the last result if we have one
-        case None if lastResult.isDefined => helper(next, startState, None, Vector(), Vector(), stream #::: Stream(lastResult.get))
-        case None => helper(t, startState, None, Vector(), Vector(), stream)
+        case None if lastResult.isDefined => lastResult.get #:: helper(next, startState, None, Vector(), Vector())
+        case None => helper(t, startState, None, Vector(), Vector())
       }
     }
 
-    helper(s.toVector, startState, None, Vector(), Vector(), Stream())
+    helper(s.toVector, startState, None, Vector(), Vector())
   }
 }
 
